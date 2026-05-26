d2447a2ab0d3c42139b46da1d47a5b881bc4ca96
Ahmed Katary <akatary01@gmail.com>
2026-05-26 20:40:42 +0000

    adding javalin api

---
+++ api/src/main/java/com/puzzlify/Api.java
@@ -0,0 +1,11 @@
+package com.puzzlify;
+
+import io.javalin.Javalin;
+
+public class Api {
+    public static void main(String[] args) {
+        var api = Javalin.create(/*config*/)
+            .get("/", ctx -> ctx.result("Hello World"))
+            .start(7070);
+    }
+}
