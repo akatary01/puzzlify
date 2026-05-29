ce19f78acd30fb037838a398d97c7796e05520c7
Ahmed Katary <akatary01@gmail.com>
2026-05-29 08:04:48 +0000

    implementing constructGrid to return a 2d array of cells where each cell is a collection of pixels

--- api/src/main/java/com/puzzlify/Api.java
+++ api/src/main/java/com/puzzlify/Api.java
@@ -1,9 +1,30 @@
 package com.puzzlify;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.puzzlify.Puzzle.Cell;
+import com.puzzlify.Puzzle.Point;
+import static com.puzzlify.Puzzle.constructGrid;
+
 import io.javalin.Javalin;
 
 public class Api {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // test image
+        File file = new File("/home/akatary/puzzlify/api/src/main/resources/billy_nips.jpeg");
+        BufferedImage image = ImageIO.read(file);
+        
+        // test that the grid outputs the correct image with the center cell all white
+        final Cell[][] grid = constructGrid(image.getHeight(), image.getWidth(), 3, 3);
+        for (final Point point : grid[1][1].coordinates()) {
+            image.setRGB(point.x(), point.y(), 255);
+        }
+        ImageIO.write(image, "png", new File("/home/akatary/puzzlify/api/src/main/resources/output.png"));
+
         var api = Javalin.create(/*config*/)
             .get("/puzzlify", ctx -> {
                 // the image, rows and cols should come from the request
--- api/src/main/java/com/puzzlify/Puzzle.java
+++ api/src/main/java/com/puzzlify/Puzzle.java
@@ -7,8 +7,37 @@ public class Puzzle {
     public record Cell(Point[] coordinates) {}
     public record Edge(Point start, Point end) {}
     
-    public static Cell[] constructGrid(int[][] image, int rows, int cols) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public static Cell[][] constructGrid(int w, int h, int rows, int cols) {
+        final Cell[][] grid = new Cell[rows][cols];
+        
+        final int a = (int) Math.floor(h/rows);
+        final int b = (int) Math.floor(w/cols);
+        for (int i = 0; i < rows; i++) {
+            final Cell[] row = new Cell[cols];
+            int rh = 0; 
+            if (i == rows - 1) {
+                rh = h % rows;
+            }
+            for (int j = 0; j < cols; j++) {
+                int rc = 0;
+                if (j == cols - 1) {
+                    rc = w % cols;
+                }
+                // cell (i, j) contains pixels with 
+                // x in [h//rows*i, h//rows*(i + 1))
+                // y in [w//cols*j, w//cols*(j + 1))
+                final Point[] coordinates = new Point[(a + rh)*(b + rc)];
+                for (int k = 0; k < a + rh; k++) {
+                    for (int l = 0; l < b + rc; l++) {
+                        coordinates[k*b + l] = new Point(k + a*i, l + b*j);
+                    }
+                }
+
+                row[j] = new Cell(coordinates);
+            }
+            grid[i] = row;
+        }
+        return grid;
     }
 
     public static Pair<Cell, Cell> cut(Edge edge, Cell cellA, Cell cellB) {
