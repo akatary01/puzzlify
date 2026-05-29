8a3804c1f87b69495da249979339ca4985abee41
Ahmed Katary <akatary01@gmail.com>
2026-05-29 08:09:48 +0000

    cleaning up constructGrid implementation

--- api/src/main/java/com/puzzlify/Puzzle.java
+++ api/src/main/java/com/puzzlify/Puzzle.java
@@ -7,28 +7,22 @@ public class Puzzle {
     public record Cell(Pixel[] coordinates) {}
     public record Edge(Pixel start, Pixel end) {}
     
-    public static Cell[][] constructGrid(int w, int h, int rows, int cols) {
+    public static Cell[][] constructGrid(final int w, final int h, final int rows, final int cols) {
         final Cell[][] grid = new Cell[rows][cols];
         
         final int a = (int) Math.floor(h/rows);
         final int b = (int) Math.floor(w/cols);
         for (int i = 0; i < rows; i++) {
             final Cell[] row = new Cell[cols];
-            int rh = 0; 
-            if (i == rows - 1) {
-                rh = h % rows;
-            }
+            // the remaining bottom pixels
+            final int rH = i == rows - 1 ? 0 : h % rows;
+
             for (int j = 0; j < cols; j++) {
-                int rc = 0;
-                if (j == cols - 1) {
-                    rc = w % cols;
-                }
-                // cell (i, j) contains pixels with 
-                // x in [h//rows*i, h//rows*(i + 1))
-                // y in [w//cols*j, w//cols*(j + 1))
-                final Pixel[] coordinates = new Pixel[(a + rh)*(b + rc)];
-                for (int k = 0; k < a + rh; k++) {
-                    for (int l = 0; l < b + rc; l++) {
+                // the remaining right-most pixels
+                int rC = j == cols - 1 ? 0 : w % cols;
+                final Pixel[] coordinates = new Pixel[(a + rH)*(b + rC)];
+                for (int k = 0; k < a + rH; k++) {
+                    for (int l = 0; l < b + rC; l++) {
                         coordinates[k*b + l] = new Pixel(k + a*i, l + b*j);
                     }
                 }
