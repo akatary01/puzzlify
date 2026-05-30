d9b79035ff1c88b17932c2ad2127181a5683d37c
Ahmed Katary <akatary01@gmail.com>
2026-05-30 20:51:27 +0000

    first complete implementation of generating puzzle

--- api/src/main/java/com/puzzlify/Api.java
+++ api/src/main/java/com/puzzlify/Api.java
@@ -3,11 +3,14 @@ package com.puzzlify;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Optional;
 
 import javax.imageio.ImageIO;
 
 import com.puzzlify.Puzzle.Cell;
+import com.puzzlify.Puzzle.Edge;
+import com.puzzlify.Puzzle.Pair;
 import com.puzzlify.Puzzle.Pixel;
 import static com.puzzlify.Puzzle.constructGrid;
 import static com.puzzlify.Puzzle.cut;
@@ -17,42 +20,86 @@ import io.javalin.Javalin;
 public class Api {
     public static void main(String[] args) throws IOException {
         // test image
-        File file = new File("../api/src/main/resources/billy_nips_bg.jpeg");
+        File file = new File("../api/src/main/resources/sp.png");
         BufferedImage image = ImageIO.read(file);
         
+        final int rows = 3;
+        final int cols = 3;
+        int[][] colors = {
+            { 0x80FF0000, 0x8000FF00, 0x800000FF },  // Row 0: Red, Green, Blue
+            { 0x80FFFF00, 0x8000FFFF, 0x80FF00FF },  // Row 1: Yellow, Cyan, Magenta
+            { 0x80FF8C00, 0x80800080, 0x8000FF80}   // Row 2: Orange, Purple, Lime
+        };
+        final Cell[][] grid = constructGrid(image, rows, cols);
+        final Cell[][] puzzle = constructGrid(image, rows, cols);
+        final HashMap<Integer, Pair<Integer, Integer>> merged = new HashMap<>();
+
         // test that the grid outputs the correct image with the center cell all white
-        final Cell[][] grid = constructGrid(image, 3, 3);
-        for(int i=1;i< 2;i++){
-            for(int j=0;j<2;j++){
-                Pixel start = new Pixel(i*image.getWidth()/3,j*image.getHeight()/3);
-                Puzzle.Edge edgeH = new Puzzle.Edge(start, start.translate(image.getWidth()/3, 0));
-                Puzzle.Edge edgeV = new Puzzle.Edge(start, start.translate(0, image.getHeight()/3));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                final Cell cellA;
+                if (puzzle[i][j] == null) {
+                    final Pair<Integer, Integer> A = merged.get(i * cols + j);
+                    cellA = puzzle[A.first()][A.second()];
+                } else {
+                    cellA = puzzle[i][j];
+                }
+                final Pair<Pixel, Pixel> boundA = grid[i][j].boundingBox();
+                System.out.println(String.format("processing cellA[%d][%d]: %s, %s", i, j, grid[i][j].size(), boundA));
 
-                //horizontal cut and update cells
-                Puzzle.Pair<Cell, Optional<Cell>> cutH =  cut(edgeH,grid[i][j],grid[i+1][j]);
-                grid[i][j] = cutH.first();
-                grid[i+1][j] = cutH.second().isPresent()? cutH.second().get():null;
-                //vertical cut and update cells
-                Puzzle.Pair<Cell, Optional<Cell>> cutV = cut(edgeV, grid[i][j],grid[i][j+1]);
-                grid[i][j] = cutV.first();
-                grid[i][j+1]= cutV.second().isPresent()? cutV.second().get():null;
+                final int a = image.getHeight() / rows;
+                final int b = image.getWidth() / cols;
+                System.out.println(String.format("(a, b): %d, %d", a, b));
 
-            }
+                final Pixel topLeft = boundA.first();
+                final Pixel bottomRight = boundA.second();
+                
+                // we do not process the last set of vertical edges
+                if (i != rows - 1) { 
+                    final Edge edgeH = new Edge(topLeft.translate(0, a - 1), bottomRight);
+                    System.out.println(String.format("edgeH: %s", edgeH));
+                    // horizontal cut and update cells
+                    Puzzle.Pair<Cell, Optional<Cell>> cutH = cut(edgeH, cellA, puzzle[i + 1][j], a, b);
+                    puzzle[i][j] = cutH.first();
+                    System.out.println(String.format("cutH cellA: %s, %s", grid[i][j].size(), grid[i][j].boundingBox()));
+                    puzzle[i + 1][j] = cutH.second().orElse(null);
+                    if (cutH.second().isEmpty()) {
+                        System.err.println("cutH cellB is merged");
+                        merged.put((i + 1) * cols + j, new Pair<>(i, j));
+                    } else {
+                        System.out.println(String.format("cutH cellB: %s, %s", grid[i + 1][j].size(), grid[i + 1][j].boundingBox()));
+                    }
+                }
+                
+                // we do not process the last set of vertical edges
+                if (j != cols - 1) {
+                    final Edge edgeV = new Edge(topLeft.translate(b - 1, 0), bottomRight);
+                    System.out.println(String.format("edgeV: %s", edgeV));
+                    //vertical cut and update cells
+                    Puzzle.Pair<Cell, Optional<Cell>> cutV = cut(edgeV, cellA, puzzle[i][j + 1], a, b);
+                    puzzle[i][j] = cutV.first();
+                    System.out.println(String.format("cutV cellA: %s, %s", grid[i][j].size(), grid[i][j].boundingBox()));
+                    puzzle[i][j + 1] = cutV.second().orElse(null);
+                    if (cutV.second().isEmpty()) {
+                        System.err.println("cutV cellB is merged");
+                        merged.put(i * cols + (j + 1), new Pair<>(i, j));
+                    } else {
+                        System.out.println(String.format("cutV cellB: %s, %s", grid[i][j + 1].size(), grid[i][j + 1].boundingBox()));
+                    }
+                }
 
-        }
-        int count=0;
-        for (final Pixel pixel : grid[0][0].pixels()) {
-            // only color in non-transparent pixels
-
-            if (pixel != null) {
-                image.setRGB(pixel.x(), pixel.y(), 255);
-                count++;
+                if (puzzle[i][j] == null) { continue; }
+                
+                for (final Pixel pixel : puzzle[i][j].pixels()) {
+                    // only color in non-transparent pixels
+                    if (pixel != null) {
+                        // System.out.println(image.getRGB(pixel.x(), pixel.y()));
+                        image.setRGB(pixel.x(), pixel.y(), colors[i % 3][j % 3]);
+                    }
+                }
             }
         }
         ImageIO.write(image, "png", new File("../api/src/main/resources/output.png"));
-        System.out.println("Count is: "+ count);
-
-
         var api = Javalin.create(/*config*/)
             .get("/puzzlify", ctx -> {
                 // the image, rows and cols should come from the request
--- api/src/main/java/com/puzzlify/Puzzle.java
+++ api/src/main/java/com/puzzlify/Puzzle.java
@@ -1,64 +1,82 @@
 package com.puzzlify;
 
+import java.awt.geom.CubicCurve2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Optional;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Puzzle {
+    private static final double MIN_VALID_EDGE_THRESHOLD = 0.2;
+
     public record Pixel(int x, int y) {
         Pixel translate(int deltaX, int deltaY) {
             return new Pixel(x() + deltaX, y() + deltaY);
         }
-        boolean between(Pixel upper, Pixel lower) {
-            return lower.x() <= x() && x() <= upper.x() && lower.y() <= y() && y() <= upper.y();
+        boolean between(@NotNull Pixel A, @NotNull Pixel B) {
+            int a = Math.min(A.x(), B.x()); int b = Math.min(A.y(), B.y());
+            int c = Math.max(A.x(), B.x()); int d = Math.max(A.y(), B.y());
+            return a <= x() && x() <= c && b <= y() && y() <= d;
         }
-        boolean equals(Pixel other) {
-//            System.out.println("comparing " + this + "to " + other);
-//            System.out.println(x() == other.x() && y() == other.y());
+        boolean equals(@NotNull Pixel other) {
             return x() == other.x() && y() == other.y();
         }
+        double distance(@NotNull Pixel pixel) {
+            return Math.sqrt(Math.pow(x() - pixel.x(), 2) + Math.pow(y() - pixel.y(), 2));
+        }
     }
-    public record Pair<T, U>(T first, U second) {}
+    public record Pair<T, U>(@NotNull T first, @NotNull U second) {}
 
-    public record Cell(Pixel[] pixels) {
-        int width() {
-            int minX = -1, maxX = -1;
-            for (Pixel p : pixels) {
-                if(p==null) continue;
-                if (minX == -1 || minX > p.x()) {
-                    minX = p.x();
-                }
-                if (maxX == -1 || maxX < p.x()) {
-                    maxX = p.x();
-                }
+    public record Cell(@NotNull Pixel[] pixels) {
+        Pair<Pixel, Pixel> boundingBox() {
+            int minX = -1, maxX = -1, minY = -1, maxY = -1;
+            for (Pixel p : pixels()) {
+                if (p == null) { continue; }
+                if (minX == -1 || minX > p.x()) { minX = p.x(); }
+                if (maxX == -1 || maxX < p.x()) { maxX = p.x(); }
+                if (minY == -1 || minY > p.y()) { minY = p.y(); }
+                if (maxY == -1 || maxY < p.y()) { maxY = p.y(); }
             }
-            return maxX - minX;
+            return new Pair<>(new Pixel(minX, minY), new Pixel(maxX, maxY));
+        }
+        int size() {
+            int i = 0;
+            for (Pixel p : pixels()) {
+                if (p != null) { i++; }
+            }
+            return i;
+        }
+        int width() {
+            final Pair<Pixel, Pixel> B = boundingBox();
+            return B.second().x() - B.first().x();
         }
         int height() {
-            int minY = -1, maxY = -1;
-            for (Pixel p : pixels) {
-                if(p==null) continue;
-                if (minY == -1 || minY > p.y()) {
-                    minY = p.y();
-                }
-                if (maxY == -1 || maxY < p.y()) {
-                    maxY = p.y();
-                }
-            }
-            return maxY - minY;
+            final Pair<Pixel, Pixel> B = boundingBox();
+            return B.second().y() - B.first().y();
         }
-        boolean contains(Pixel pixel) {
-            for (Pixel p : pixels) {
-                if(p==null) continue;
-                if (p.equals(pixel)) {
-                    System.out.println(p +", " +pixel);
+        boolean contains(@NotNull Pixel pixel) {
+            for (Pixel p : pixels()) {
+                if (p != null && p.equals(pixel)) {
                     return true;
                 }
             }
             return false;
         }
+        Pixel closest(@NotNull Pixel pixel) {
+            double minD = Double.MAX_VALUE;
+            Pixel closestP = null;
+            for (Pixel p : pixels()) {
+                if (p == null) { continue; }
 
-        Cell merge(Cell other) {
+                double d = p.distance(pixel);
+                if (p.distance(pixel) < minD) {
+                    minD = d; closestP = p;
+                }
+            }
+            return closestP;
+        }
+        Cell merge(@NotNull Cell other) {
             final Pixel[] mergedPixels = new Pixel[pixels().length + other.pixels().length];
             
             int i;
@@ -70,16 +88,16 @@ public class Puzzle {
         }
     }
     public enum EdgeType { HORIZONTAL, VERTICAL }
-    public record Edge(Pixel start, Pixel end) {
+    public record Edge(@NotNull Pixel start, @NotNull Pixel end) {
         Pixel midpoint() {
-            return new Pixel((int) Math.floor((start.x() + end.x()) / 2), (int) Math.floor((start.y() + end.y()) / 2));
+            return new Pixel(((start.x() + end.x()) / 2), ((start.y() + end.y()) / 2));
         }
         EdgeType type() {
             // assumption: the edge is valid
             if (start.x() == end.x()) {
                 return EdgeType.VERTICAL;
             } 
-            return EdgeType.VERTICAL;
+            return EdgeType.HORIZONTAL;
         }
     }
     
@@ -89,8 +107,8 @@ public class Puzzle {
         final int h = image.getHeight();
         final int w = image.getWidth();
         
-        final int a = (int) Math.floor(h/rows);
-        final int b = (int) Math.floor(w/cols);
+        final int a = h / rows;
+        final int b = w / cols;
         for (int i = 0; i < rows; i++) {
             final Cell[] row = new Cell[cols];
             // the remaining bottom pixels
@@ -103,10 +121,15 @@ public class Puzzle {
                 for (int k = 0; k < a + rH; k++) {
                     for (int l = 0; l < b + rC; l++) {
                         // only add non-transparent pixels to the cell
-                        if (image.getColorModel().hasAlpha() && ((image.getRGB(l + b*j, k + a*i) >> 24) & 0xff) == 255) {
+                        if (
+                            !image.getColorModel().hasAlpha() ||
+                            (
+                                image.getColorModel().hasAlpha() && 
+                                ((image.getRGB(l + b*j, k + a*i) >> 24) & 0xff) == 255
+                            )
+                        ) {
                             pixels[k*b + l] = new Pixel(l + b*j, k + a*i);
                         }
-                            
                     }
                 }
 
@@ -117,50 +140,59 @@ public class Puzzle {
         return grid;
     }
 
-    public static Pair<Cell, Optional<Cell>> cut(Edge edge, Cell cellA, Cell cellB) {
+    public static Pair<Cell, Optional<Cell>> cut(Edge edge, @NotNull Cell cellA, Cell cellB, int avgH, int avgW) {
         // if the edge is too small (i.e < half the expected length, we merge the cells)
         // if one of the cells is too small we merge the cells
-        if(cellA == null) return new Pair<>(cellA, Optional.of(cellB));
-        if(cellB==null) return new Pair<>(cellA,Optional.of(cellB));
-        if (!isEdgeValid(edge, cellA) || !isCellValid(cellA) || !isCellValid(cellB)) {
-           System.out.println("invalid edge: " + edge);
-           System.out.println("CellA: is cell valid: "+ isCellValid(cellA)+", is edge valid: " +
-                   isEdgeValid(edge,cellA));
-            System.out.println("CellB: is cell valid: "+ isCellValid(cellB));
+        if (cellB == null) { 
+            return new Pair<>(cellA, Optional.empty()); 
+        }
 
-            return new Pair<>(cellA.merge(cellB), Optional.empty());
-        } 
-        final int direction = Math.random() > 0.5 ? 1 : -1;
+        edge = new Edge(cellA.closest(edge.start()), cellA.closest(edge.end()));
+        int direction = cellA.size() < cellB.size() ? 1 : -1;
         final Pixel midpoint = edge.midpoint();
         Pixel upper, lower;
+
+        double edgeLength = edge.start().distance(edge.end());
+        int cutSize = (int) (0.25 * edgeLength);
         switch (edge.type()) {
             case HORIZONTAL -> { 
-                lower = midpoint.translate(0, -50);
-                upper = midpoint.translate(direction * 50, 50);
+                lower = midpoint.translate(-cutSize, 0);
+                upper = midpoint.translate(cutSize, direction * ((int) ((0.35 * Math.random() + 0.15) * cellA.height())));
             }
             case VERTICAL -> { 
-                lower = midpoint.translate(-50, 0);
-                upper = midpoint.translate(50, direction * 50);
+                lower = midpoint.translate(0, -cutSize);
+                upper = midpoint.translate(direction * ((int) ((0.35 * Math.random() + 0.15) * cellA.width())), cutSize);
             }
             default -> throw new IllegalArgumentException();
         }
-
-        Pair<Cell, ArrayList<Pixel>> targets;
+        System.out.println(String.format("(lower, upper): %s, %s", lower, upper));
+        final Pair<Cell, ArrayList<Pixel>> targets;
+        final CubicCurve2D curve = curvedEdge(edge, upper, lower);
         if (direction == -1) {
-            targets = removePixels(cellA, upper, lower);
+            targets = removePixels(cellA, curve, edge);
+            System.out.println(String.format("targets: (, %d)", targets.second().size()));
             return new Pair<>(targets.first(), Optional.of(addPixels(cellB, targets.second())));
         } else {
-            targets = removePixels(cellB, upper, lower);
+            targets = removePixels(cellB, curve, edge);
+            System.out.println(String.format("targets: (, %d)", targets.first().size()));
             return new Pair<>(addPixels(cellA, targets.second()), Optional.of(targets.first()));
         }
     }
-    private static Pair<Cell, ArrayList<Pixel>> removePixels(Cell cell, Pixel upper, Pixel lower) {
+    private static CubicCurve2D curvedEdge(Edge edge, Pixel upper, Pixel lower) {
+        return new CubicCurve2D.Double(
+            edge.start().x(), edge.start().y(), 
+            lower.x(), lower.y(), 
+            upper.x(), upper.y(), 
+            edge.end().x(), edge.end().y()
+        );
+    }
+    private static Pair<Cell, ArrayList<Pixel>> removePixels(Cell cell, CubicCurve2D curve, Edge edge) {
         final Pixel[] pixels = new Pixel[cell.pixels().length];
         final ArrayList<Pixel> pixelsRemoved = new ArrayList<>();
         for (int i = 0; i < cell.pixels().length; i++) {
             final Pixel pixel = cell.pixels()[i];
-            if(pixel==null) continue;
-            if (!pixel.between(upper, lower)) {
+            if (pixel == null) continue;
+            if (curve.contains(pixel.x(), pixel.y()) || pixel.between(edge.start(), edge.end())) {
                 pixelsRemoved.add(pixel);
             } else {
                 pixels[i] = pixel;
@@ -174,14 +206,11 @@ public class Puzzle {
         System.arraycopy(pixels.toArray(), 0, pixelsAppended, cell.pixels().length, pixels.size());
         return new Cell(pixelsAppended);
     }
-    private static boolean isEdgeValid(Edge edge, Cell cell) {
-        System.out.print("Mid point: " + cell.contains(edge.midpoint()));
-        System.out.print(" ,Edge Start: " + cell.contains(edge.start()));
-        System.out.println(" ,Edge end: " + cell.contains(edge.end()));
-        return cell.contains(edge.midpoint()) && (cell.contains(edge.start()) || cell.contains(edge.end()));
+    private static boolean isEdgeValid(Edge edge, int avgH, int avgW) {
+        return edge.start().distance(edge.end()) > MIN_VALID_EDGE_THRESHOLD * (edge.type() == EdgeType.VERTICAL ? avgH : avgW);
     }
-    private static boolean isCellValid(Cell cell) {
-        return cell.width() > 50 && cell.height() > 50;
+    private static boolean isCellValid(Cell cell, int avgH, int avgW) {
+        return cell.width() > MIN_VALID_EDGE_THRESHOLD * avgW  && cell.height() > MIN_VALID_EDGE_THRESHOLD * avgH;
     }
     private Puzzle() {}
 }
