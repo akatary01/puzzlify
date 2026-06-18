928a5f3d5f8c4f384b4dad5918f5c265897a6b85
Eemzz <e.jaludi@gmail.com>
2026-06-18 16:30:43 -0700

    GET setup

--- puzzlify.js
+++ puzzlify.js
@@ -15,8 +15,36 @@ class CustomElement extends HTMLElement {
     }
 }
 
-function puzzlify(rows, cols){
+async function puzzlify(rows, cols){
     console.log(rows, cols);
+
+    //post 
+    const imageView = document.getElementById("image-view");
+    var fd = new FormData();
+    fd.append('image', document.getElementById("input-file").files[0]);
+
+    const qstr = new URLSearchParams({rows, cols}).toString(); //makes a query string 
+    const url = new URL(`http://localhost:7070/puzzlify?${qstr}`);
+
+    try {
+        const response = await fetch(url,{
+            method: 'post',
+            body: fd
+        });
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    
+        const puzzle = await response; //dont need to make it json
+
+        imageView.style.backgroundImage = `url(${puzzle})`;
+        imageView.style.backgroundRepeat = `no-repeat`;
+        imageView.style.backgroundPosition = `center`;
+        imageView.textContent = "";
+
+        console.log(data);
+    } catch (error) {
+        console.error("Fetch failed:", error);
+    }
+    
     //make a request to localhost:7070/puzzlify
     //pass in uploaded image as file and rows and cols as ints
     //await the response then preview the output image
